fa01ea541735f64e2983a3ef56c7ab5ab91b3d04
arvinytan <arvinytan@tencent.com>
2020-12-18 20:40:59 +0800

    添加最小编辑距离

--- AlgorithmPractice/PalindromeSolution.hpp
+++ AlgorithmPractice/PalindromeSolution.hpp
@@ -124,5 +124,71 @@ public:
     }
 };
 
+/**
+ 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+
+ 你可以对一个单词进行如下三种操作：
+
+ 插入一个字符
+ 删除一个字符
+ 替换一个字符
+  
+
+ 示例 1：
+
+ 输入：word1 = "horse", word2 = "ros"
+ 输出：3
+ 解释：
+ horse -> rorse (将 'h' 替换为 'r')
+ rorse -> rose (删除 'r')
+ rose -> ros (删除 'e')
+ 示例 2：
+
+ 输入：word1 = "intention", word2 = "execution"
+ 输出：5
+ 解释：
+ intention -> inention (删除 't')
+ inention -> enention (将 'i' 替换为 'e')
+ enention -> exention (将 'n' 替换为 'x')
+ exention -> exection (将 'n' 替换为 'c')
+ exection -> execution (插入 'u')
+  
+
+ 提示：
+
+ 0 <= word1.length, word2.length <= 500
+ word1 和 word2 由小写英文字母组成
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/edit-distance
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class MinEditDistance {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = (int)word1.length();
+        int len2 = (int)word2.length();
+        
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+        for (int i = 1; i <= len1; ++i) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= len2; ++j) {
+            dp[0][j] = j;
+        }
+        
+        for (int row = 0; row < len1 ; ++row) {
+            for (int column = 0; column < len2; ++column) {
+                if (word1[row] == word2[column]) {
+                    dp[row + 1][column + 1] = dp[row][column];
+                } else {
+                    dp[row + 1][column + 1] = std::min(dp[row][column],  std::min(dp[row][column + 1], dp[row + 1][column])) + 1;
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+};
+
 
 #endif /* PalindromeSolution_hpp */
