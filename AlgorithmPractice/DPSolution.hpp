c397d1cfb2d30d08ecd14fea4f2b9340fc8620bd
arvinytan <arvinytan@tencent.com>
2020-12-26 09:45:54 +0800

    并查集：朋友圈

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -167,15 +167,15 @@ public:
             int firstPerson = dislikes[i][0];
             int secondPerson = dislikes[i][1];
             
-            int firstLikeRoot = ufind(parent, firstPerson);
-            int secondLikeRoot = ufind(parent, secondPerson);
+            int firstLikeRoot = uf_find(parent, firstPerson);
+            int secondLikeRoot = uf_find(parent, secondPerson);
             
             if (firstLikeRoot == secondLikeRoot) {
                 return false;
             }
             
-            int firstHateRoot = ufind(parent, firstPerson + N);
-            int secondHateRoot = ufind(parent, secondPerson + N);
+            int firstHateRoot = uf_find(parent, firstPerson + N);
+            int secondHateRoot = uf_find(parent, secondPerson + N);
             
             parent[firstHateRoot] = secondLikeRoot;
             parent[secondHateRoot] = firstPerson;
@@ -183,12 +183,77 @@ public:
         return true;
     }
     
-    int ufind(vector<int> &parent, int x) {
+    int uf_find(vector<int> &parent, int x) {
         while (parent[x] != x) {
             parent[x] = parent[parent[x]];
             x = parent[x];
         }
         return x;
+    }
+    
+    /**
+     班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+
+     给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+
+      
+
+     示例 1：
+
+     输入：
+     [[1,1,0],
+      [1,1,0],
+      [0,0,1]]
+     输出：2
+     解释：已知学生 0 和学生 1 互为朋友，他们在一个朋友圈。
+     第2个学生自己在一个朋友圈。所以返回 2 。
+     示例 2：
+
+     输入：
+     [[1,1,0],
+      [1,1,1],
+      [0,1,1]]
+     输出：1
+     解释：已知学生 0 和学生 1 互为朋友，学生 1 和学生 2 互为朋友，所以学生 0 和学生 2 也是朋友，所以他们三个在一个朋友圈，返回 1 。
+      
+
+     提示：
+
+     1 <= N <= 200
+     M[i][i] == 1
+     M[i][j] == M[j][i]
+
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode-cn.com/problems/friend-circles
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    int findCircleNum(vector<vector<int>>& M) {
+        int N = (int)M.size();
+        vector<int> parent(N, 0);
+        for (int i = 0; i <= N; ++i) {
+            parent[i] = i;
+        }
+        
+        int circles = N;
+        for (int i = 0; i < N; ++i) {
+            for (int j = i + 1; j < N; ++j) {
+                if (M[i][j] == 1) {
+                    uf_union(parent, i, j);
+                    circles -= 1;
+                }
+            }
+        }
+        return circles;
+    }
+    
+    bool uf_union(vector<int> &parent, int x, int y) {
+        int fx = uf_find(parent, x);
+        int fy = uf_find(parent, y);
+        if (fx != fy) {
+            parent[fx] = fy;
+        }
+        return fx != fy;
     }
 };
 
