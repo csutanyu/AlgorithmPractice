1cecf90b857b2473b91b3f5047ec2f0ffa3bfd68
arvinytan <arvinytan@tencent.com>
2020-12-26 22:00:15 +0800

    990. 等式方程的可满足性

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -194,6 +194,19 @@ public:
         return x;
     }
     
+    bool uf_union(vector<int> &parent, int x, int y) {
+        int fx = uf_find(parent, x);
+        int fy = uf_find(parent, y);
+        if (fx != fy) {
+            parent[fx] = fy;
+        }
+        return fx != fy;
+    }
+    
+    bool uf_connected(vector<int> &parent, int x, int y) {
+        return uf_find(parent, x) == uf_find(parent, y);
+    }
+    
     /**
      班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
 
@@ -257,16 +270,6 @@ public:
         return circles;
     }
     
-    bool uf_union(vector<int> &parent, int x, int y) {
-        int fx = uf_find(parent, x);
-        int fy = uf_find(parent, y);
-        if (fx != fy) {
-            parent[fx] = fy;
-        }
-        return fx != fy;
-    }
-    
-    
     
     /**
      给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
@@ -322,6 +325,80 @@ public:
             }
         }
         return maxLength;
+    }
+    
+    /**
+     990. 等式方程的可满足性
+     
+     给定一个由表示变量之间关系的字符串方程组成的数组，每个字符串方程 equations[i] 的长度为 4，并采用两种不同的形式之一："a==b" 或 "a!=b"。在这里，a 和 b 是小写字母（不一定不同），表示单字母变量名。
+
+     只有当可以将整数分配给变量名，以便满足所有给定的方程时才返回 true，否则返回 false。
+
+      
+
+     示例 1：
+
+     输入：["a==b","b!=a"]
+     输出：false
+     解释：如果我们指定，a = 1 且 b = 1，那么可以满足第一个方程，但无法满足第二个方程。没有办法分配变量同时满足这两个方程。
+     示例 2：
+
+     输入：["b==a","a==b"]
+     输出：true
+     解释：我们可以指定 a = 1 且 b = 1 以满足满足这两个方程。
+     示例 3：
+
+     输入：["a==b","b==c","a==c"]
+     输出：true
+     示例 4：
+
+     输入：["a==b","b!=c","c==a"]
+     输出：false
+     示例 5：
+
+     输入：["c==c","b==d","x!=z"]
+     输出：true
+      
+
+     提示：
+
+     1 <= equations.length <= 500
+     equations[i].length == 4
+     equations[i][0] 和 equations[i][3] 是小写字母
+     equations[i][1] 要么是 '='，要么是 '!'
+     equations[i][2] 是 '='
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode-cn.com/problems/satisfiability-of-equality-equations
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    
+    bool equationsPossible(vector<string>& equations) {
+        vector<int> parent(26, 0);
+        for (int i = 0; i < 26; ++i) {
+            parent[i] = i;
+        }
+        
+        // 等式构建不并查集
+        for (string &s : equations) {
+            if (s[1] == '=') {
+                int firstIndex = s[0] - 'a';
+                int secondIndex = s[3] - 'a';
+                uf_union(parent, firstIndex, secondIndex);
+            }
+        }
+        
+        // 查不等式，是否在同一个连通分支或者树中
+        for (string &s : equations) {
+            if (s[1] == '!') {
+                int firstIndex = s[0] - 'a';
+                int secondIndex = s[3] - 'a';
+                if (uf_connected(parent, firstIndex, secondIndex)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
 
--- AlgorithmPractice/ViewController.mm
+++ AlgorithmPractice/ViewController.mm
@@ -45,15 +45,23 @@ using namespace std;
     
     DPSolution dps;
     //        [100,4,200,1,3,2]
-    vector<int> v;
-    v.push_back(100);
-    v.push_back(4);
-    v.push_back(200);
-    v.push_back(1);
-    v.push_back(3);
-    v.push_back(2);
+//    vector<int> v;
+//    v.push_back(100);
+//    v.push_back(4);
+//    v.push_back(200);
+//    v.push_back(1);
+//    v.push_back(3);
+//    v.push_back(2);
+//
+//    int c = dps.longestConsecutive(v);
     
-    int c = dps.longestConsecutive(v);
+//    ["c==c","b==d","x!=z"]
+    
+    vector<string> equations;
+    equations.push_back("c==c");
+    equations.push_back("b==d");
+    equations.push_back("x!=z");
+    bool e = dps.equationsPossible(equations);
     int x;
 }
 
