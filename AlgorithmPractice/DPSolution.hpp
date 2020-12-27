5f6a82226a053b66e7d3f7545acb481b1e8ef417
arvinytan <arvinytan@tencent.com>
2020-12-27 16:20:10 +0800

    152. 乘积最大子数组

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -725,4 +725,50 @@ public:
     }
 };
 
+/**
+ 152. 乘积最大子数组
+ 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+  
+
+ 示例 1:
+
+ 输入: [2,3,-2,4]
+ 输出: 6
+ 解释: 子数组 [2,3] 有最大乘积 6。
+ 示例 2:
+
+ 输入: [-2,0,-1]
+ 输出: 0
+ 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ */
+class MaxProduct {
+    
+public:
+    int maxProduct(vector<int>& nums) {
+        int size = (int)nums.size();
+        if (size == 0) return 0;
+        vector<pair<int, int>> dp(size, pair<int, int>(0, 0));
+        dp[size - 1].first = nums[size - 1];
+        dp[size - 1].second = nums[size - 1];
+        
+        int res = nums[size - 1];
+        for (int i = size - 2; i >= 0; --i) {
+            // 与最大值乘积
+            int calFirst = nums[i] * dp[i+1].first;
+            
+            // 与最小值乘积
+            int calSecond = nums[i] * dp[i+1].second;
+            
+            int curMax = max(nums[i], max(calFirst, calSecond));
+            int curMin = min(nums[i], min(calFirst, calSecond));
+            dp[i].first = curMax;
+            dp[i].second = curMin;
+            
+            res = max(res, curMax);
+        }
+        return res;
+    }
+};
+
 #endif /* DPSolution_hpp */
