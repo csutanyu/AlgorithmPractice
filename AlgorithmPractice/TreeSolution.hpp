f76a57d896f5516b4785c72e3be21f24ec635160
arvinytan <arvinytan@tencent.com>
2020-12-28 11:19:59 +0800

    145. 二叉树的后序遍历:递归

--- AlgorithmPractice/TreeSolution.hpp
+++ AlgorithmPractice/TreeSolution.hpp
@@ -146,6 +146,40 @@ public:
             }
         }
     }
+    
+    /**
+     145. 二叉树的后序遍历
+     给定一个二叉树，返回它的 后序 遍历。
+
+     示例:
+
+     输入: [1,null,2,3]
+        1
+         \
+          2
+         /
+        3
+
+     输出: [3,2,1]
+     进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+     通过次数174,128提交次数236,046
+     */
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        recursionpostorderTraversal(res, root);
+        
+        return res;
+    }
+    
+    void recursionpostorderTraversal(vector<int>& res, TreeNode* root) {
+        if (root == NULL) return;
+        
+        recursionpostorderTraversal(res, root->left);
+        recursionpostorderTraversal(res, root->right);
+        res.push_back(root->val);
+    }
+    
 };
 
 
