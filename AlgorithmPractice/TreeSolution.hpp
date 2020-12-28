f097e60ef236135a3363891014295b5bcd3800f0
arvinytan <arvinytan@tencent.com>
2020-12-28 19:55:15 +0800

    107. 二叉树的层序遍历 II

--- AlgorithmPractice/TreeSolution.hpp
+++ AlgorithmPractice/TreeSolution.hpp
@@ -285,6 +285,53 @@ public:
         return res;
     }
     
+    /**
+     107. 二叉树的层序遍历 II
+     给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+     例如：
+     给定二叉树 [3,9,20,null,null,15,7],
+
+         3
+        / \
+       9  20
+         /  \
+        15   7
+     返回其自底向上的层序遍历为：
+
+     [
+       [15,7],
+       [9,20],
+       [3]
+     ]
+     */
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root == NULL) {
+            return res;
+        }
+        queue<TreeNode * > nodeQueue;
+        nodeQueue.push(root);
+        while (!nodeQueue.empty()) {
+            res.push_back(vector<int>());
+            size_t currentLevelSize = nodeQueue.size();
+            for (int i = 0; i < currentLevelSize; ++i) {
+                TreeNode *current = nodeQueue.front();
+                res.back().push_back(current->val);
+                nodeQueue.pop();
+                if (current->left != NULL) {
+                    nodeQueue.push(current->left);
+                }
+                if (current->right != NULL) {
+                    nodeQueue.push(current->right);
+                }
+            }
+        }
+        
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
 };
 
 
