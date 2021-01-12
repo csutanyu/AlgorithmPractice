ffc5ff60e8bae17a0b7442c9e7ee16947402a170
arvinytan <arvinytan@tencent.com>
2021-01-12 13:23:11 +0800

    1722. 执行交换操作后的最小汉明距离

--- AlgorithmPractice/LeetCodeCommons.hpp
+++ AlgorithmPractice/LeetCodeCommons.hpp
@@ -17,6 +17,7 @@
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -160,6 +161,88 @@ public:
     
     bool uf_connected(vector<int> &parent, int x, int y) {
         return parent[x] == parent[y];
+    }
+    
+    
+    
+    /**
+     1722. 执行交换操作后的最小汉明距离
+     给你两个整数数组 source 和 target ，长度都是 n 。还有一个数组 allowedSwaps ，其中每个 allowedSwaps[i] = [ai, bi] 表示你可以交换数组 source 中下标为 ai 和 bi（下标从 0 开始）的两个元素。注意，你可以按 任意 顺序 多次 交换一对特定下标指向的元素。
+
+     相同长度的两个数组 source 和 target 间的 汉明距离 是元素不同的下标数量。形式上，其值等于满足 source[i] != target[i] （下标从 0 开始）的下标 i（0 <= i <= n-1）的数量。
+
+     在对数组 source 执行 任意 数量的交换操作后，返回 source 和 target 间的 最小汉明距离 。
+
+      
+
+     示例 1：
+
+     输入：source = [1,2,3,4], target = [2,1,4,5], allowedSwaps = [[0,1],[2,3]]
+     输出：1
+     解释：source 可以按下述方式转换：
+     - 交换下标 0 和 1 指向的元素：source = [2,1,3,4]
+     - 交换下标 2 和 3 指向的元素：source = [2,1,4,3]
+     source 和 target 间的汉明距离是 1 ，二者有 1 处元素不同，在下标 3 。
+     示例 2：
+
+     输入：source = [1,2,3,4], target = [1,3,2,4], allowedSwaps = []
+     输出：2
+     解释：不能对 source 执行交换操作。
+     source 和 target 间的汉明距离是 2 ，二者有 2 处元素不同，在下标 1 和下标 2 。
+     示例 3：
+
+     输入：source = [5,1,2,4,3], target = [1,5,4,2,3], allowedSwaps = [[0,4],[4,2],[1,3],[1,4]]
+     输出：0
+      
+
+     提示：
+
+     n == source.length == target.length
+     1 <= n <= 105
+     1 <= source[i], target[i] <= 105
+     0 <= allowedSwaps.length <= 105
+     allowedSwaps[i].length == 2
+     0 <= ai, bi <= n - 1
+     ai != bi
+     通过次数2,068提交次数5,228
+     https://leetcode-cn.com/problems/minimize-hamming-distance-after-swap-operations/
+     */
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int len = (int)source.size();
+        
+        // 初始化并查集
+        vector<int> parent(len, 0);
+        for (int i = 0; i < len; ++i) {
+            parent[i] = i;
+        }
+        
+        // 构造并查集
+        for (vector<int> &pair : allowedSwaps) {
+            uf_union(parent, pair[0], pair[1]);
+        }
+         
+        unordered_map<int/*index*/, unordered_multiset<int>> hasMap;
+        for (int i = 0; i < len; ++i) {
+            int rootIndex = uf_find(parent, i);
+            if (hasMap.count(rootIndex) == 0) {
+                hasMap[rootIndex] = unordered_multiset<int>();
+            }
+            hasMap[rootIndex].insert(source[i]);
+        }
+        
+        int distanceCount = 0;
+        for (int i = 0; i < len; ++i) {
+            int rootIndex = uf_find(parent, i);
+            auto& uset = hasMap[rootIndex];
+            auto it = uset.find(target[i]);
+            if (it == uset.end()) {
+                ++distanceCount;
+            } else {
+                uset.erase(it);
+            }
+        }
+        
+        return distanceCount;
     }
 };
 
