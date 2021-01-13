b12388f093e825e25e6a27aef1fd35e94dbb8b01
arvinytan <arvinytan@tencent.com>
2021-01-13 20:25:25 +0800

    15. 三数之和

--- AlgorithmPractice/LeetCodeCommons.hpp
+++ AlgorithmPractice/LeetCodeCommons.hpp
@@ -332,5 +332,67 @@ public:
 };
 
 
+/**
+ 15. 三数之和
+ 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+ 注意：答案中不可以包含重复的三元组。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [-1,0,1,2,-1,-4]
+ 输出：[[-1,-1,2],[-1,0,1]]
+ 示例 2：
+
+ 输入：nums = []
+ 输出：[]
+ 示例 3：
+
+ 输入：nums = [0]
+ 输出：[]
+  
+
+ 提示：
+
+ 0 <= nums.length <= 3000
+ -105 <= nums[i] <= 105
+ 通过次数397,776提交次数1,303,354
+ 在真实的面试中遇到过这道题？
+ 贡献者
+ LeetCode
+ */
+class ThreeSum {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = (int)nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ret;
+        for (int first = 0; first < n; ++first) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int third = n - 1;
+            for (int second = first + 1; second < n; ++second) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                while (second < third && nums[first] + nums[second] + nums[third] > 0) {
+                    --third;
+                }
+                if (second == third) {
+                    break;
+                }
+                if (nums[first] + nums[second] + nums[third] == 0) {
+                    ret.push_back({nums[first], nums[second], nums[third]});
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+
 
 #endif /* LeetCodeCommons_hpp */
