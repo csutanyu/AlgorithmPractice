0db0346b769b0ae37ed9abce4a11e071213ddf82
arvinytan <arvinytan@tencent.com>
2021-01-15 14:18:58 +0800

    添加测试target

---
+++ AlgorithmPracticeTests/SortTests.mm
@@ -0,0 +1,32 @@
+//
+//  SortTests.m
+//  AlgorithmPracticeTests
+//
+//  Created by arvintan on 2021/1/15.
+//  Copyright © 2021 arvintan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Algorithms4Chapter2.hpp"
+
+@interface SortTests : XCTestCase
+
+@end
+
+@implementation SortTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testSort {
+    
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
