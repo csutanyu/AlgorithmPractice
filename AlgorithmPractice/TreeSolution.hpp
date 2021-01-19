d823f1dd551e700d6931198ba9a48419599e8565
arvinytan <arvinytan@tencent.com>
2021-01-19 21:48:55 +0800

    剑指 Offer 28. 对称的二叉树

--- AlgorithmPractice/TreeSolution.hpp
+++ AlgorithmPractice/TreeSolution.hpp
@@ -14,6 +14,9 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
 #include <map>
 #include <stack>
 #include <queue>
@@ -574,6 +577,78 @@ public:
             return false;
         }
         return isSubStructureFromRoot(A->left, B->left) && isSubStructureFromRoot(A->right, B->right);
+    }
+    
+    /**
+     剑指 Offer 28. 对称的二叉树
+     请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+     例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+         1
+        / \
+       2   2
+      / \ / \
+     3  4 4  3
+     但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+         1
+        / \
+       2   2
+        \   \
+        3    3
+
+      
+
+     示例 1：
+
+     输入：root = [1,2,2,3,4,4,3]
+     输出：true
+     示例 2：
+
+     输入：root = [1,2,2,null,3,null,3]
+     输出：false
+     https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
+     */
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        
+        deque<TreeNode *> curQue;
+        curQue.push_back(root);
+        
+        bool res = true;
+        while (!curQue.empty()) {
+            int N = (int)curQue.size();
+            
+            int iterationCount = N / 2;
+            deque<TreeNode *>::iterator begin = curQue.begin();
+            deque<TreeNode *>::reverse_iterator rbegin = curQue.rbegin();
+            do {
+                if ((*begin)->val != (*rbegin)->val) {
+                    res = false;
+                    break;
+                }
+                ++begin;
+                ++rbegin;
+            } while (--iterationCount > 0);
+            
+            if (!res) {
+                break;
+            }
+            
+            for (int i = 0; i < N; ++i) {
+                TreeNode *firstNode = curQue.front();
+                // 添加首部节点的子节点
+                if (NULL != firstNode->left) {
+                    curQue.push_back(firstNode->left);
+                }
+                if (NULL != firstNode->right) {
+                    curQue.push_back(firstNode->right);
+                }
+                curQue.pop_front();
+            }
+        }
+        return res;
     }
 };
 
