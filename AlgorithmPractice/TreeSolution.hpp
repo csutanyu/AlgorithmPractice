2d780f5a6c3b4235e205019535068620c07d7e81
arvinytan <arvinytan@tencent.com>
2021-01-22 20:59:52 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先

--- AlgorithmPractice/TreeSolution.hpp
+++ AlgorithmPractice/TreeSolution.hpp
@@ -861,6 +861,103 @@ public:
         double solo = max(0.0, l.first - r.first - r.second) + max(0.0, r.first - l.first - l.second);
         return make_pair(solo + root->val, total - solo);
     }
+    
+    /**
+     剑指 Offer 68 - II. 二叉树的最近公共祖先
+     给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+     百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+     例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+     示例 1:
+
+     输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+     输出: 3
+     解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+     示例 2:
+
+     输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+     输出: 5
+     解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+      
+
+     说明:
+
+     所有节点的值都是唯一的。
+     p、q 为不同节点且均存在于给定的二叉树中。
+     注意：本题与主站 236 题相同：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+     https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+     */
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        unordered_map<TreeNode *, pair<bool, bool>> hmap;
+        
+        TreeNode *res = nullptr;
+    
+        stack<TreeNode *> tStack;
+        TreeNode * cur = root;
+        TreeNode * pre = nullptr;
+        
+        while (!tStack.empty() || cur != nullptr) {
+            if (cur != nullptr) {
+                tStack.push(cur);
+                cur = cur->left;
+            } else {
+                TreeNode *node = tStack.top();
+                if (nullptr == node->right || pre == node->right) {
+                    pair<bool, bool> leftFlags = {false, false};
+                    if (node->left != nullptr && hmap.count(node->left)) {
+                        leftFlags = hmap[node->left];
+                    }
+                    
+                    pair<bool, bool> rightFlags = {false, false};
+                    if (node->right != nullptr && hmap.count(node->right)) {
+                        rightFlags = hmap[node->right];
+                    }
+                    leftFlags.first = leftFlags.first || rightFlags.first;
+                    leftFlags.second = leftFlags.second || rightFlags.second;
+                    if (isTheSameNode(node, p)) {
+                        leftFlags.first = true;
+                    }
+                    if (isTheSameNode(node, q)) {
+                        leftFlags.second = true;
+                    }
+                    if (leftFlags.first && leftFlags.second) {
+                        res = node;
+                        break;
+                    }
+                    hmap[node] = leftFlags;
+                    tStack.pop();
+                    pre = node;
+                } else {
+                    cur = node->right;
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    inline bool isTheSameNode(TreeNode *n1, TreeNode *n2) {
+        return n1->val == n2->val;
+    }
+    
+//  递归  剑指 Offer 68 - II. 二叉树的最近公共祖先
+    TreeNode* ans;
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) || ((root->val == p->val || root->val == q->val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root->val == p->val || root->val == q->val);
+    }
 };
 
 
