c080ca4ccd657c6ce9e5bd27b211c29cf6404d4c
arvinytan <arvinytan@tencent.com>
2021-01-27 20:53:24 +0800

    剑指 Offer 59 - I. 滑动窗口的最大值

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -1480,4 +1480,55 @@ public:
         return dp;
     }
 };
+
+/**
+ 剑指 Offer 59 - I. 滑动窗口的最大值
+ 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+
+ 示例:
+
+ 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ 输出: [3,3,5,5,6,7]
+ 解释:
+
+   滑动窗口的位置                最大值
+ ---------------               -----
+ [1  3  -1] -3  5  3  6  7       3
+  1 [3  -1  -3] 5  3  6  7       3
+  1  3 [-1  -3  5] 3  6  7       5
+  1  3  -1 [-3  5  3] 6  7       5
+  1  3  -1  -3 [5  3  6] 7       6
+  1  3  -1  -3  5 [3  6  7]      7
+  
+
+ 提示：
+
+ 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+
+ 注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/
+
+.https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/
+ */
+
+class MaxSlidingWindowSolution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int N = nums.size();
+        vector<int> dp(N, 0);
+        if (N == 0) return dp;
+        if (N < k) return dp; //错误情况
+
+        for (int i = 0; i < N; ++i) {
+            dp[i] = nums[i];
+        }
+        for (int wd = 2; wd <= k; ++wd) {
+            for (int i = 0; i <= N - wd; ++i) {
+                dp[i] = max(dp[i], dp[i+1]);
+            }
+        }
+
+        vector<int> res(dp.begin(), (dp.begin() + N - k + 1));
+        return res;
+    }
+};
 #endif /* DPSolution_hpp */
