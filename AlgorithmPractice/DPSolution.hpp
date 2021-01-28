943869c4f556e4383d3384ab0c8bff97359f53f2
arvinytan <arvinytan@tencent.com>
2021-01-28 20:39:17 +0800

    剑指 Offer 13. 机器人的运动范围

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -1574,4 +1574,68 @@ public:
            return res;
     }
 };
+
+/**
+ 剑指 Offer 13. 机器人的运动范围
+ 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+
+  
+
+ 示例 1：
+
+ 输入：m = 2, n = 3, k = 1
+ 输出：3
+ 示例 2：
+
+ 输入：m = 3, n = 1, k = 0
+ 输出：1
+ 提示：
+
+ 1 <= n,m <= 100
+ 0 <= k <= 20
+ 通过次数89,763提交次数175,503
+ https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/submissions/
+ */
+class RobotMovintCountSolution {
+public:
+    int movingCount(int m, int n, int k) {
+       return bfs(m, n, k);
+    }
+
+    int bfs(int m, int n, int k) {
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int, int>> que;
+        que.push(make_pair(0, 0));
+        int res = 0;
+        while (!que.empty()) {
+            pair<int, int> cur = que.front();
+            que.pop();
+            if (visited[cur.first][cur.second] || positionSum(cur.first, cur.second) > k) {
+                continue;
+            }
+            ++res;
+            visited[cur.first][cur.second] = true;
+            if (cur.first + 1 < m) {
+                que.push(make_pair(cur.first + 1, cur.second));
+            }
+            if (cur.second + 1 < n) {
+                que.push(make_pair(cur.first, cur.second + 1));
+            }
+        }
+        return res;
+    }
+
+    int positionSum(int x, int y) {
+        int res = 0;
+        while (x != 0) {
+           res += x % 10;
+           x = x / 10;
+        }
+        while (y != 0) {
+            res += y % 10;
+            y = y / 10;
+        }
+        return res;
+    }
+};
 #endif /* DPSolution_hpp */
