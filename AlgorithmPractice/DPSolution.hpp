2cc0b0a8b00f927df26c9fe5a652a0d7c5394e2f
arvinytan <arvinytan@tencent.com>
2021-02-24 13:21:07 +0800

    931. 下降路径最小和

--- AlgorithmPractice/DPSolution.hpp
+++ AlgorithmPractice/DPSolution.hpp
@@ -1638,4 +1638,73 @@ public:
         return res;
     }
 };
+
+/**
+ 931. 下降路径最小和
+ 给你一个 n x n 的 方形 整数数组 matrix ，请你找出并返回通过 matrix 的下降路径 的 最小和 。
+
+ 下降路径 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。具体来说，位置 (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1) 。
+
+  
+
+ 示例 1：
+
+ 输入：matrix = [[2,1,3],[6,5,4],[7,8,9]]
+ 输出：13
+ 解释：下面是两条和最小的下降路径，用加粗标注：
+ [[2,1,3],      [[2,1,3],
+  [6,5,4],       [6,5,4],
+  [7,8,9]]       [7,8,9]]
+ 示例 2：
+
+ 输入：matrix = [[-19,57],[-40,-5]]
+ 输出：-59
+ 解释：下面是一条和最小的下降路径，用加粗标注：
+ [[-19,57],
+  [-40,-5]]
+ 示例 3：
+
+ 输入：matrix = [[-48]]
+ 输出：-48
+  
+
+ 提示：
+
+ n == matrix.length
+ n == matrix[i].length
+ 1 <= n <= 100
+ -100 <= matrix[i][j] <= 100
+ https://leetcode-cn.com/problems/minimum-falling-path-sum/
+ */
+class MinFallingPathSumSolution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int rowCount = (int)matrix.size();
+        if (rowCount == 0) return 0;
+        int columnCount = (int)(matrix[0].size());
+        vector<vector<int>> dp(rowCount, vector<int>(columnCount));
+        for (int i = 0; i < columnCount; ++i) {
+            dp[rowCount - 1][i] = matrix[rowCount - 1][i];
+        }
+        
+        for (int row = rowCount - 2; row >= 0; --row) {
+            for (int column = 0; column < columnCount; ++column) {
+                int temp = dp[row + 1][column];
+                if (column - 1 >= 0) {
+                    temp = min(dp[row + 1][column - 1], temp);
+                }
+                if (column + 1 < columnCount) {
+                    temp = min(dp[row + 1][column + 1], temp);
+                }
+                dp[row][column] = matrix[row][column] + temp;
+            }
+        }
+        
+        int ret = INT_MAX;
+        for (int j = 0; j < columnCount; ++j) {
+            ret = min(ret, dp[0][j]);
+        }
+        return ret;
+    }
+};
 #endif /* DPSolution_hpp */
